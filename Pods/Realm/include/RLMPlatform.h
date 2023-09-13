6ff6b34dd6b8d3c9b63844380850849b41c5d680
heshamabdelhamead <34060487+heshamabdelhamead@users.noreply.github.com>
2023-09-13 21:16:03 +0300

    pod 3'

---
+++ Pods/Realm/include/RLMPlatform.h
@@ -0,0 +1,5 @@
+//#define REALM_IOPLATFORMUUID @"WARNING: The active Xcode command line tools, as returned by 'xcode-select -p', are not from Xcode.
+//         The newest version of Xcode will be used instead.
+//5F025415-1D1F-5103-902A-ABF9618B35C4"
+#define REALM_IOPLATFORMUUID @"5F025415-1D1F-5103-902A-ABF9618B35C4"
+
